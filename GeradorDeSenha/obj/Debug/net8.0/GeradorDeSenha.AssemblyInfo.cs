50cbed708c98aab2b4c2934a080fea07e1b69959
Vinicius Wendt <wendtvinicius1801@gmail.com>
2025-01-22 13:49:54 -0300

    add senhas

--- GeradorDeSenha/obj/Debug/net8.0/GeradorDeSenha.AssemblyInfo.cs
+++ GeradorDeSenha/obj/Debug/net8.0/GeradorDeSenha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea6ea7726845d040ecd1f885d01c0502a985911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1214c16ba39b424a49be58b392e9df9fadd7fff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
